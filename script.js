8e53cb90a6b053b966a1d930e799a5230cbe96ea
Levlio123456 <levi@manicke.eu>
2025-10-20 22:30:22 -0400

    Quick push

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ correctness = document.getElementById('correctness');
 scoreCounter = document.getElementById('scoreCounter');
 
 score = 0;
+totalGames = 0;
 
 branches = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
 randomBranch = Math.floor(Math.random() * 12);
@@ -11,14 +12,16 @@ guess.innerHTML = "Find branch: " + branches[randomBranch];
 
 
 function branchClicked(branch) {
+	totalGames++;
 	if (branch === branches[randomBranch]) {
 		console.log('Correct!');
 		correctness.innerHTML = "Correct!";
 		score++;
-		scoreCounter.innerHTML = score;
 	} else {
 		correctness.innerHTML = "Incorrect!";
 	}
+	scoreCounter.innerHTML = score + ' / ' + totalGames + '<br>' + Math.floor(score / totalGames * 100) + '%';
+
 	randomBranch = Math.floor(Math.random() * 12);
 	console.log(branches[randomBranch]);
 	guess.innerHTML = "Find branch: " + branches[randomBranch];
