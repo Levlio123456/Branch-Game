285ba2dd3899c81c76d80eebe6fe27958780c4a4
Levlio123456 <levi@manicke.eu>
2025-10-20 22:15:47 -0400

    Score added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 guess = document.getElementById('guess');
 correctness = document.getElementById('correctness');
+scoreCounter = document.getElementById('scoreCounter');
+
+score = 0;
 
 branches = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
 randomBranch = Math.floor(Math.random() * 12);
@@ -11,6 +14,8 @@ function branchClicked(branch) {
 	if (branch === branches[randomBranch]) {
 		console.log('Correct!');
 		correctness.innerHTML = "Correct!";
+		score++;
+		scoreCounter.innerHTML = score;
 	} else {
 		correctness.innerHTML = "Incorrect!";
 	}
